ceca73426b37dbea433b7aa590b0bc061a00b529
adema <adema@itu.dk>
2022-03-29 18:49:42 +0200

    Fix .metadata

--- Exercise examples/SQLBase/Development/org.mdse.minisql.query.interpreter/src/org/mdse/minisql/query/interpreter/MSQLQueryInterpreterException.java
+++
@@ -1,9 +0,0 @@
-package org.mdse.minisql.query.interpreter;
-
-public class MSQLQueryInterpreterException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public MSQLQueryInterpreterException(String message) {
-		super(message);
-	}
-}
