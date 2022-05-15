273dc7c5993063e3a4cd2e83c3924a9e7f03517c
Jakub <jakub.sowa@websensa.com>
2022-05-15 19:02:00 +0200

    fix: made the aird look reasonable

--- Development/pizzacution.query.dsl/xtend-gen/pizzacution/query/dsl/formatting2/LanguageFormatter.java
+++ Development/pizzacution.query.dsl/xtend-gen/pizzacution/query/dsl/formatting2/LanguageFormatter.java
@@ -4,40 +4,46 @@
 package pizzacution.query.dsl.formatting2;
 
 import java.util.Arrays;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.formatting2.AbstractFormatter2;
 import org.eclipse.xtext.formatting2.IFormattableDocument;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.xbase.lib.Extension;
+import pizzacution.query.SelectQuery;
+import pizzacution.query.ThatClause;
+import pizzacution.query.ThatDirective;
+import pizzacution.query.WhatClause;
 
 @SuppressWarnings("all")
 public class LanguageFormatter extends AbstractFormatter2 {
-  protected void _format(final /* SelectQuery */Object selectQuery, @Extension final IFormattableDocument document) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nwhatClause cannot be resolved"
-      + "\nformat cannot be resolved"
-      + "\nthatClause cannot be resolved"
-      + "\nformat cannot be resolved");
+  protected void _format(final SelectQuery selectQuery, @Extension final IFormattableDocument document) {
+    document.<WhatClause>format(selectQuery.getWhatClause());
+    EList<ThatClause> _thatClause = selectQuery.getThatClause();
+    for (final ThatClause thatClause : _thatClause) {
+      document.<ThatClause>format(thatClause);
+    }
   }
   
-  protected void _format(final /* ThatClause */Object thatClause, @Extension final IFormattableDocument document) {
-    throw new Error("Unresolved compilation problems:"
-      + "\nthatDirective cannot be resolved"
-      + "\nformat cannot be resolved");
+  protected void _format(final ThatClause thatClause, @Extension final IFormattableDocument document) {
+    document.<ThatDirective>format(thatClause.getThatDirective());
   }
   
   public void format(final Object selectQuery, final IFormattableDocument document) {
     if (selectQuery instanceof XtextResource) {
       _format((XtextResource)selectQuery, document);
       return;
+    } else if (selectQuery instanceof SelectQuery) {
+      _format((SelectQuery)selectQuery, document);
+      return;
+    } else if (selectQuery instanceof ThatClause) {
+      _format((ThatClause)selectQuery, document);
+      return;
     } else if (selectQuery instanceof EObject) {
       _format((EObject)selectQuery, document);
       return;
     } else if (selectQuery == null) {
       _format((Void)null, document);
-      return;
-    } else if (selectQuery != null) {
-      _format(selectQuery, document);
       return;
     } else if (selectQuery != null) {
       _format(selectQuery, document);
